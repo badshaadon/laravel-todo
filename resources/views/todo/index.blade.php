16ad85972ce429fe41b027a36ce78939f6c343ff
Ali Naeem <alitheflyingtiger111@gmail.com>
2020-05-12 12:57:18 +0500

    task compeleted button updated

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -14,10 +14,14 @@
                 <div class="card-body">
                     <ul class="list-group">
                         @foreach ($todos as $todo)
-                            <li class="list-group-item">
+                            <li class="list-group-item my-2" style="border: 5px solid gray;">
                                 {{ $todo->name }}
-                                <a class="btn btn-primary float-right" href="/todos/{{ $todo->id }}">View</a>
-                                <a class="btn btn-primary float-right" href="/todos/{{ $todo->id }}/compeleted">Compelete</a>
+                                <a class="btn btn-primary float-right ml-3" href="/todos/{{ $todo->id }}">View</a>
+                                @if (!$todo->compeleted)
+                                    <a class="btn btn-success float-right" href="/todos/{{ $todo->id }}/compeleted">Compelete</a>
+                                @else
+                                    <a class="btn btn-secondary btn-disabled float-right" href="#">Compeleted</a>
+                                @endif
                             </li>
                         @endforeach
                     </ul>
