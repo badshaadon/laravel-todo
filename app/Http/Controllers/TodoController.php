8d77ae400605df889e14b23a05d53d16d2513667
Ali Naeem <alitheflyingtiger111@gmail.com>
2020-05-12 11:40:40 +0500

    index method added

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index');
+        $todos = Todo::all();
+        return view('todo.index')->with('todos', $todos);
     }
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -5,5 +5,22 @@
 @endsection
 
 @section('content')
-    All Todos
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    All Todos
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach ($todos as $todo)
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
